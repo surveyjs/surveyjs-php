36e9b5276205313203d22a18666be8f21df9b9bc
tsv2013 <tsv2013@users.noreply.github.com>
2017-10-03 10:37:29 +0300

    Changed results header

--- src/results.js
+++ src/results.js
@@ -1,12 +1,12 @@
 function SurveyManager(baseUrl, accessKey) {
     var self = this;
     var url = new URL(document.URL);
-    var surveyId = url.searchParams.get("id");        
+    self.surveyId = url.searchParams.get("id");        
     self.results = ko.observableArray();
 
     self.loadResults = function () {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', baseUrl + '/results?postId=' + surveyId);
+        xhr.open('GET', baseUrl + '/results?postId=' + self.surveyId);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.onload = function () {
             var result = xhr.response ? JSON.parse(xhr.response) : {};
