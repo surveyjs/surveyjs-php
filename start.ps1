26b6084d15c1e32d86bd767724200373ffc4e153
TSV <tsv2013@users.noreply.github.com>
2023-01-19 11:50:45 +0300

    Fixed setup scripts

--- start.ps1
+++
@@ -1,2 +0,0 @@
-docker run --name dbsrv -p 5432:5432 -d postgresql-db
-docker run --name appsrv --link dbsrv:dataserver -p 8000:80 -d -v <ABSOLUTE PATH TO THE SRC FOLDER>:/var/www/site/ surveyjs-php
--- startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i 's/\/home\/site\/wwwroot/\/home\/site\/wwwroot\/public/g' /etc/nginx/sites-available/default && service nginx reload
